415f33fd25dff831288d45914c68db55520f2b24
realtweety <157921800+realtweety@users.noreply.github.com>
2026-03-04 16:59:48 -0600

    Delete wilburt_ai_micro_server directory

--- wilburt_ai_micro_server/app/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/utils/__init__.py
-"""
-WilburtAI Micro Server - Utilities Package
-"""
-
-from .validators import validate_chat_id, validate_message_content
-from .helpers import generate_title, sanitize_filename
-
-__all__ = ['validate_chat_id', 'validate_message_content', 'generate_title', 'sanitize_filename']
