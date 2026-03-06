ff362097c673e3a009f41c1c3c76d4690f53ea9a
realtweety <157921800+realtweety@users.noreply.github.com>
2026-03-05 22:48:37 -0600

    Delete wilburt_ai_micro_server directory

--- wilburt_ai_micro_server/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/models/__init__.py
-"""
-WilburtAI Micro Server - Models Package
-"""
-
-from .chat import User, Chat, Message
-
-__all__ = ['User', 'Chat', 'Message']
--- wilburt_ai_micro_server/config/_init_.py
+++
@@ -1,8 +0,0 @@
-# config/__init__.py
-"""
-WilburtAI Micro Server - Configuration Package
-"""
-
-from .settings import Config
-
-__all__ = ['Config']
