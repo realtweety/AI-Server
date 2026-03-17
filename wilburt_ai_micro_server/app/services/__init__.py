b2be291f3feea397c97ca518244afafe140b2557
realtweety <157921800+realtweety@users.noreply.github.com>
2026-03-17 03:46:21 -0500

    Delete wilburt_ai_micro_server directory

--- wilburt_ai_micro_server/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/services/__init__.py
-"""
-WilburtAI Micro Server - Services Package
-"""
-
-from .chat_service import ChatService
-from .lm_service import LMService
-
-__all__ = ['ChatService', 'LMService']
